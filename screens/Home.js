ef9be4d9b2769bf08c5f78112f62de44ffe8f782
medobarakat <ahmedbarakat2401@gmail.com>
2022-05-03 15:02:35 +0200

    start in home

--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,151 @@
-import React from 'react';
+import React from "react";
 import {
-    View,
-    Text,
-    TouchableOpacity
-} from 'react-native';
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+  FlatList,
+  ScrollView,
+  TouchableWithoutFeedback,
+  ImageBackground,
+} from "react-native";
+// import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import Animated from "react-native-reanimated";
+import {
+  dummyData,
+  icons,
+  images,
+  theme,
+  COLORS,
+  SIZES,
+  FONTS,
+} from "../constants";
+import { newSeason } from "../constants/dummy";
 
 const Home = ({ navigation }) => {
+  const newSeasonScrollX = React.useRef(new Animated.Value(0)).current;
+
+  function renderHeader() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("MovieDetail")}
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingHorizontal: SIZES.padding,
+        }}
+      >
+        {/* first icon */}
+        <TouchableOpacity
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            width: 50,
+            height: 50,
+          }}
+          onPress={() => console.log("profile")}
+        >
+          <Image
+            source={images.profile_photo}
+            style={{
+              width: 40,
+              height: 40,
+              borderRadius: 20,
+            }}
+          />
+        </TouchableOpacity>
+        {/* screen mirror */}
+        <TouchableOpacity
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            width: 50,
+            height: 50,
+          }}
+        >
+          <Image
+            source={icons.airplay}
+            style={{
+              width: 25,
+              height: 25,
+              tintColor: COLORS.primary,
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  function renderNewSessionSection() {
+    return (
+      <Animated.FlatList
+        horizontal
+        pagingEnabled
+        snapToAlignment="center"
+        snapToInterval={SIZES.width}
+        scrollEventThrottle={16}
+        decelerationRate={0}
+        contentContainerStyle={{
+          marginTop: SIZES.radius,
+        }}
+        data={dummyData.newSeason}
+        keyExtractor={(item) => `${item.id}`}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: newSeasonScrollX } } }],
+          { useNativeDriver: false }
+        )}
+        renderItem={({ item, index }) => {
+          return (
+            <TouchableWithoutFeedback
+              onPress={() =>
+                navigation.navigate("MovieDetail", { selectedMovie: Item })
+              }
             >
-                <Text>Navigate to MovieDetail</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+              <View
+                style={{
+                  width: SIZES.width,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <ImageBackground
+                  resizeMode="cover"
+                  source={item.thumbnail}
+                  style={{
+                    width: SIZES.width * 0.85,
+                    height: SIZES.width * 0.85,
+                    justifyContent: "flex-end",
+                  }}
+                ></ImageBackground>
+              </View>
+            </TouchableWithoutFeedback>
+          );
+        }}
+      />
+    );
+  }
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.black,
+      }}
+    >
+      {/* header */}
+      {renderHeader()}
+      {/* start of content */}
+      <ScrollView
+        horizontal
+        style={{
+          paddingBottom: 100,
+        }}
+      >
+        {renderNewSessionSection()}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
