bd99d4262fc7627ec72eed95d920edf2cfcefdf5
medobarakat <ahmedbarakat2401@gmail.com>
2022-05-03 23:59:10 +0200

    finishing details

--- screens/MovieDetail.js
+++ screens/MovieDetail.js
@@ -1,3 +1,4 @@
+import LinearGradient from "react-native-linear-gradient";
 import React from "react";
 import {
   View,
@@ -11,6 +12,7 @@ import {
 } from "react-native";
 import { ProgressBar } from "../components";
 import { COLORS, SIZES, FONTS, icons } from "../constants";
+
 const MovieDetail = ({ navigation, route }) => {
   const [selectedMovie, setSelectedMovie] = React.useState(null);
   React.useEffect(() => {
@@ -92,8 +94,168 @@ const MovieDetail = ({ navigation, route }) => {
           }}
         >
           {renderHeaderBar()}
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "flex-end",
+            }}
+          >
+            <LinearGradient
+              start={{ x: 0, y: 0 }}
+              end={{ x: 0, y: 1 }}
+              colors={["transparent", "black"]}
+              style={{
+                width: "100%",
+                height: 150,
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
+            >
+              {/* season */}
+              <Text
+                style={{
+                  color: COLORS.white,
+                  ...FONTS.body4,
+                }}
+              >
+                {selectedMovie?.details?.season}
+              </Text>
+
+              {/* name */}
+              <Text
+                style={{
+                  color: COLORS.white,
+                  ...FONTS.h1,
+                  marginTop: SIZES.base,
+                }}
+              >
+                {selectedMovie?.name}
+              </Text>
+            </LinearGradient>
+          </View>
         </View>
       </ImageBackground>
+    );
+  }
+
+  function renderCategory() {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: SIZES.base,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {/* age */}
+        <View style={[styles.categoryContainer, { marginLeft: 0 }]}>
+          <Text
+            style={{
+              color: COLORS.white,
+              ...FONTS.h4,
+            }}
+          >
+            {selectedMovie?.details?.age}
+          </Text>
+        </View>
+
+        {/* genre */}
+        <View
+          style={[
+            styles.categoryContainer,
+            { paddingHorizontal: SIZES.padding },
+          ]}
+        >
+          <Text
+            style={{
+              color: COLORS.white,
+              ...FONTS.h4,
+            }}
+          >
+            {selectedMovie?.details?.genre}
+          </Text>
+        </View>
+        {/* rating */}
+        <View style={styles.categoryContainer}>
+          <Image
+            source={icons.star}
+            resizeMode="contain"
+            style={{
+              width: 15,
+              height: 15,
+            }}
+          />
+          <Text
+            style={{
+              marginLeft: SIZES.base,
+              color: COLORS.white,
+              ...FONTS.h4,
+            }}
+          >
+            {selectedMovie?.details?.ratings}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+
+  function renderMovieDetail() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          paddingHorizontal: SIZES.padding,
+          marginTop: SIZES.padding,
+          justifyContent: "space-around",
+        }}
+      >
+        {/* title , running time and progressbar */}
+        <View>
+          {/* title */}
+          <View
+            style={{
+              flexDirection: "row",
+            }}
+          >
+            <Text style={{ flex: 1, color: COLORS.white, ...FONTS.h4 }}>
+              {selectedMovie?.details?.currentEpisode}
+            </Text>
+            <Text style={{ color: COLORS.lightGray, ...FONTS.body4 }}>
+              {selectedMovie?.details?.runningTime}
+            </Text>
+          </View>
+          {/* progressbar */}
+          <ProgressBar
+            containerStyle={{ marginTop: SIZES.radius }}
+            barStyle={{ height: 5, borderRadius: 3 }}
+            barPercentage={selectedMovie?.details?.progress}
+          />
+        </View>
+
+        {/* button */}
+        <TouchableOpacity
+          style={{
+            height: 60,
+            alignItems: "center",
+            justifyContent: "center",
+            marginBottom: Platform.OS === "ios" ? SIZES.padding * 2 : 0,
+            backgroundColor: COLORS.primary,
+            borderRadius: 15,
+          }}
+        >
+          <Text
+            style={{
+              color: COLORS.white,
+              ...FONTS.h2,
+            }}
+          >
+            {selectedMovie?.details?.progress === "0%"
+              ? "Watch Now"
+              : "Continue Watching"}
+          </Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 
@@ -102,9 +264,28 @@ const MovieDetail = ({ navigation, route }) => {
       contentContainerStyle={{ flex: 1, backgroundColor: COLORS.black }}
       style={{ backgroundColor: COLORS.black }}
     >
+      {/* header */}
       {renderHeaderSection()}
+      {/* category&ratings */}
+      {renderCategory()}
+
+      {/* moviedetails */}
+      {renderMovieDetail()}
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  categoryContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: SIZES.base,
+    paddingHorizontal: SIZES.base,
+    paddingVertical: 3,
+    borderRadius: SIZES.base,
+    backgroundColor: COLORS.gray1,
+  },
+});
 
 export default MovieDetail;
